e8513a8a58440457457fb68215388d466e3c6015
Usuario <Usuario@DESKTOP-CDFF6GA>
2024-10-25 18:30:32 -0600

    Se agregaron validaciones en producto

--- src/main/java/CRUD/CRUD/ENTIDAD/Producto.java
+++ src/main/java/CRUD/CRUD/ENTIDAD/Producto.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -28,15 +30,35 @@ public class Producto{
     private Integer codigo;
     
     @Column(name = "cantidad")
-    private int cantidad;
+    @NotNull(message = "La cantidad no debe estar vacio")
+    @Min(value = 0, message = "El valor debe ser mayor a 0")
+    private Integer cantidad;
+
     @Column(name = "descripcion")
+    @NotBlank(message = "La descripcion no debe estar vacia") 
     private String descripcion;
+    
     @Column(name = "categoria")
     private String categoria;
+    
     @Column(name = "precio_unitario")
-    private float precio_unitario;
+    @NotNull(message = "El precio no puede estar vacio")
+    @DecimalMin(value = "0.01", message = "El precio debe ser mayo a 0")
+    private Double precio_unitario;
+    
     @Column(name = "costo_unitario")
-    private float costo_unitario;
+    @NotNull(message = "El costo no puede estar vacio")
+    @DecimalMin(value = "0.01", message = "El costo debe ser mayo a 0")
+    private Double costo_unitario;
+    
+    //Validacion que el costo no sea mayor al precio
+    @AssertTrue(message = "El costo no puede ser mayo al precio")
+    public boolean isCostoValido(){
+        if (precio_unitario == null || costo_unitario==null) {
+            return true;
+        }
+        return costo_unitario <= precio_unitario;
+    }
     
     //sirve para hacer relacionn de un cliente a muchos productos
     @ManyToOne
@@ -46,7 +68,7 @@ public class Producto{
     public Producto() {
     }
 
-    public Producto(Long id, Integer codigo, int cantidad, String descripcion, String categoria, float precio_unitario, float costo_unitario) {
+    public Producto(Long id, Integer codigo, Integer cantidad, String descripcion, String categoria, Double precio_unitario, Double costo_unitario) {
         this.id = id;
         this.codigo = codigo;
         this.cantidad = cantidad;
@@ -56,7 +78,7 @@ public class Producto{
         this.costo_unitario = costo_unitario;
     }
 
-    public Producto(Integer codigo, int cantidad, String descripcion, String categoria, float precio_unitario, float costo_unitario) {
+    public Producto(Integer codigo, Integer cantidad, String descripcion, String categoria, Double precio_unitario, Double costo_unitario) {
         this.codigo = codigo;
         this.cantidad = cantidad;
         this.descripcion = descripcion;
@@ -81,14 +103,14 @@ public class Producto{
         this.codigo = codigo;
     }
 
-    
-    public int getCantidad() {
+    public Integer getCantidad() {
         return cantidad;
     }
 
-    public void setCantidad(int cantidad) {
+    public void setCantidad(Integer cantidad) {
         this.cantidad = cantidad;
     }
+
 
     public String getDescripcion() {
         return descripcion;
@@ -114,20 +136,19 @@ public class Producto{
         this.cliente = cliente;
     }
 
-
-    public float getPrecio_unitario() {
+    public Double getPrecio_unitario() {
         return precio_unitario;
     }
 
-    public void setPrecio_unitario(float precio_unitario) {
+    public void setPrecio_unitario(Double precio_unitario) {
         this.precio_unitario = precio_unitario;
     }
 
-    public float getCosto_unitario() {
+    public Double getCosto_unitario() {
         return costo_unitario;
     }
 
-    public void setCosto_unitario(float costo_unitario) {
+    public void setCosto_unitario(Double costo_unitario) {
         this.costo_unitario = costo_unitario;
     }
 
